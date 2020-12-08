84b23c70c21f8d8888e621097058b8e323c2ef23
Tahir Kamcili <tahir.kamcili@tu-dortmund.de>
2020-12-08 22:26:19 +0100

    V204 fertig

--- V204/Daten/V204.py
+++ V204/Daten/V204.py
@@ -49,14 +49,13 @@ plt.figure()
 plt.xlabel(r'$t \, [s]$')
 plt.ylabel(r'$T  \, [K]$')
 
-plt.plot(t_1, T_1_1, label=r'$T_1$')
-plt.plot(t_1, T_4_1, label=r'$T_4$')
-plt.plot(t_1, T_5_1, label=r'$T_5$')
-plt.plot(t_1, T_8_1, label=r'$T_8$')
-
+plt.plot(t_1, T_1_1, label=r'$T1 = Messing(dick)$')
+plt.plot(t_1, T_4_1, label=r'$T4 = Messing(dünn)$')
+plt.plot(t_1, T_5_1, label=r'$T5 = Aluminium$')
+plt.plot(t_1, T_8_1, label=r'$T8 = Edelstahl$')
 plt.legend()
 plt.tight_layout()
-plt.savefig('grafic.pdf')
+plt.savefig('Daten/grafic.pdf')
 
 #statische Methode 700s:
 print(f'Die Temperatur bei T1 beträgt: {T_1_1[7000]}, \n Die Temperatur bei T4 beträgt: {T_4_1[7000]}, \n Die Temperatur bei T5 beträgt: {T_5_1[7000]}, \n Die Temperatur bei T8 beträgt: {T_1_1[7000]}, \n')
@@ -104,6 +103,10 @@ def betrag_z(A):
     if(A < 0):
         return -A
     return A
+
+
+def lam(kap, f, roh, c):
+    np.sqrt((4*kap*np.pi*f)/(roh*c))
 
 
 T_12_1 = T_1_1 - T_2_1
@@ -191,57 +194,53 @@ x1, _ = find_peaks(T_1_2, distance = 15)
 x2, _ = find_peaks(-T_1_2, distance = 15)
 A11 = T_1_2[x1] - T_1_2[x2]
 A1 = ds(A11)
-print(f'Die mittlere Amplitude von T1 beträgt A1 = {A1} ')
+print(f'Die mittlere Amplitude von T1 beträgt A1 = {A11} ')
 
 #Berechnung von der Amplitude T2
 x3, _ = find_peaks(T_2_2, distance = 15)
 x4, _ = find_peaks(-T_2_2, distance = 15)
 A22 = T_2_2[x3] - T_2_2[x4]
 A2 = ds(A22)
-print(f'Die mittlere Amplitude von T2 beträgt A2 = {A2} ')
+print(f'Die Amplitude von T2 beträgt A2 = {A22} ')
 
 #Berechnung der mittleren Phasendifferenz
 
 t_ph = (t_2[x1] - t_2[x3] + t_2[x2] - t_2[x4])/2
 
 
-print(f'Die durschnittliche Phasendifferenz beträgt t = {ds(t_ph)}')
+print(f'Die Phasendifferenz beträgt t = {t_ph}')
 
 #Berechnung der mittleren Wärmeleitfähigkeit
 
-print(f'Die mittleren Wärmeleitfähigkeit beträgt k = {kappa(8400, 377, 0.029, ds(t_ph), A1, A2) } (Messing dick) ' )
+print(f'Die Wärmeleitfähigkeit beträgt k = {kappa(8400, 377, 0.029, t_ph, A11, A22) } (Messing dick) ' )
 
 #Aluminium
 #Berechnung von der Amplitude T5
-x5, _ = find_peaks(T_5_2, distance = 15)
-x5 = betrag(x5)
-x6, _ = find_peaks(-T_5_2, distance = 15)
-x6 = betrag(x6)
-A55 = x5 - x6
-A55 = betrag(A55)
+x1, _ = find_peaks(T_5_2, distance = 15)
+x2, _ = find_peaks(-T_5_2, distance = 15)
+A55 = T_5_2[x1][0:-2] - T_5_2[x2][0:-2]
 A5 = ds(A55)
 print(f'Die mittlere Amplitude von T5 beträgt A5 = {A5} ')
 
 #Berechnung von der Amplitude T6
-x5, _ = find_peaks(T_6_2, distance = 15)
-x5 = betrag(x5)
-x6, _ = find_peaks(-T_6_2, distance = 15)
-x6 = betrag(x6)
-A66 = - x5[0:-1] + x6
-A66 = betrag(A66)
+x3, _ = find_peaks(T_6_2, distance = 15)
+x4, _ = find_peaks(-T_6_2, distance = 15)
+A66 = T_6_2[x3][0:-3] - T_6_2[x4][0:-2]
 A6 = ds(A66)
 print(f'Die mittlere Amplitude von T6 beträgt A6 = {A6} ')
 
 #Berechnung der mittleren Phasendifferenz
+t_ph1 = (t_2[x1] - t_2[x3])
+t_ph2 = (t_2[x2][0:-1] - t_2[x4])
+t_ph = (t_ph1[0:-1] + t_ph2)/2
 
-t_ph = T_6_2[A66] - T_5_2[A55][0:-1]
 
 print(f'Die durschnittliche Phasendifferenz beträgt t = {ds(t_ph)}')
 
 #Berechnung der mittleren Wärmeleitfähigkeit
 
 print(f'Die mittleren Wärmeleitfähigkeit beträgt k = {kappa(2700, 896, 0.03, ds(t_ph), A5, A6) } (Aluminium) ' )
-
+print(f'Die mittleren Wellenlänge beträgt lambda = {lam(kappa(2700, 896, 0.03, ds(t_ph), A5, A6),0.01228,2700,896) } (Aluminium)')
 
 
 # Dynamisch Periode 200s
@@ -261,31 +260,40 @@ plt.savefig('grafic4.pdf')
 #Amplituden
 
 #Berechnung von der Amplitude T7
-x5, _ = find_peaks(T_7_3, distance = 15)
-x5 = betrag(x5)
-x6, _ = find_peaks(-T_7_3, distance = 15)
-x6 = betrag(x6)
-A77 = - x5[0:-1] + x6
-A77 = betrag(A77)
+x1, _ = find_peaks(T_7_3, distance = 15)
+x2, _ = find_peaks(-T_7_3, distance = 15)
+A77 = T_7_3[x1][0:-1] - T_7_3[x2]
+A7 = ds(A77)
 
 print(f'Die mittlere Amplitude von T7 beträgt A7 = {A77} ')
 
+
+
 #Berechnung von der Amplitude T6
-x5, _ = find_peaks(T_8_3, distance = 15)
-x5 = betrag(x5)
-x6, _ = find_peaks(-T_8_3, distance = 15)
-x6 = betrag(x6)
-A88 =  x5 - x6
-A88 = betrag(A88)
+x3, _ = find_peaks(T_8_3, distance = 15)
+x4, _ = find_peaks(-T_8_3, distance = 15)
+
+#T_8_3[x3] = [301.93, 308.58, 314.37, 319.04, 322.85]
+#T_8_3[x4] = [294.36, 301.86, 308.57, 313.84, 318.18]
+
+x3 = [94, 190, 287, 384]
+x4 = [109, 213, 316, 418]
+
+A88 = T_8_3[x3] - T_8_3[x4]
+A8 = ds(A88)
+
 
 print(f'Die mittlere Amplitude von T8 beträgt A8 = {A88} ')
 
 #Berechnung der mittleren Phasendifferenz
 
-t_ph = - T_8_3[A88][0:-2] + T_7_3[A77]
+t_ph1 = (t_3[x1][0:-1] - t_3[x3])
+t_ph2 = (t_3[x2] - t_3[x4])
+t_ph = (t_ph1 + t_ph2)/2
+
 print(f'Die Phasendifferenz beträgt t = {t_ph}')
 print(f'Die durschnittliche Phasendifferenz beträgt t = {ds(t_ph)}')
 
 #Berechnung der Wärmeleitfähigkeit
 
-print(f'Die Anzahl an ermittelten Perioden ist {len(A77)} Wärmeleitfähigkeit beträgt k = {kappa(8000, 4182, 0.031, ds(t_ph), A77, A88[0:-2]) } (Edelstahl) ' )
+print(f'Die Anzahl an ermittelten Perioden ist {len(A77)} Wärmeleitfähigkeit beträgt k = {kappa(8000, 500, 0.031, ds(t_ph), A77, A88) } (Edelstahl) ' )
