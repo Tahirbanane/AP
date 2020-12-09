c31734ade2dea3646928f2ad7708c0da6dcb4c86
Tahir Kamcili <tahir.kamcili@tu-dortmund.de>
2020-12-09 21:14:30 +0100

    auswertung

--- V302/Daten/V302.py
+++ V302/Daten/V302.py
@@ -5,10 +5,19 @@ import sympy
 import statistics
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
+from scipy.signal import find_peaks
 
 def ds(A):
     return statistics.mean(A)
 
+from functools import reduce
+
+def get_minmax(values):
+    return reduce(
+        lambda mm, v: (min(v, mm[0]), max(v, mm[1])),
+        values,
+        (values[0],) * 2
+    )
 
 #Aufgabe a bereits händisch berechnet
 
@@ -33,12 +42,52 @@ print(f'Lx = {L2*R3/R4}')
 
 #Aufgabe d
 
+R2, R3, R4 = np.genfromtxt('Daten/d.txt', unpack = True)
+
 C4 = 399 * 10**(-9)
 
 print(f'Rx = {R2*R3/R4}')
-print(f'Rx Fehler = {R2*(0.03*R3)/R4 + R2*R3/((0.03*R4)**2)}')
+print(f'Rx Fehler = {R2*(0.03*R3)/R4 + R2*R3*(0.03*R4)/(R4**2)}')
 print(f'Lx = {R2*R3*C4} +- 3% Fehler')
 
 #Aufgabe e
 
+# R = 1000 Ohm
+# w = 2pi f
+f, UBr, Us = np.genfromtxt('Daten/e.txt', unpack = True)
 
+UBr /= 2
+Us /= 2
+
+w = 2*np.pi*f
+R = 1000
+C = (399 + 450)/2 #mittelwert der beiden Kondensatoren
+C *= 10**(-9)
+
+w0 = 1/(R*C)
+
+Q = np.sqrt(1/9*((w/w0)**2-1)**2/((1-(w/w0)**2)**2+9*(w/w0)**2))
+
+plt.figure()
+plt.ylabel(r'$U_\text{Br}/U_s$')
+plt.xlabel(r'$\omega/\omega_0$')
+plt.xscale('log')
+plt.plot(w/w0, UBr/Us,'b.' ,label = r'$gemessen$')
+plt.plot(w/w0, Q,'g--' ,label = r'$errechnet$')
+plt.legend()
+plt.tight_layout()
+plt.savefig('Daten/grafic.pdf')
+
+x2, _ = find_peaks(-UBr/Us, distance = 15)
+
+print(f'UBr/Us minima: {get_minmax(UBr/Us[x2])}')
+
+U2 = UBr/(np.sqrt(1/9*((2)**2-1)**2/((1-(2)**2)**2+9*(2)**2)))
+
+print(f'U2: {U2}')
+
+k = U2/Us
+
+print(f'k: {k,ds(k)}')
+
+ds(k)
